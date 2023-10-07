6a525c549d41837f829fadf4982f76af38a18974
andre bovis <andre_bovis@hotmail.com>
2023-10-07 04:49:15 -0300

    obtendo usuario por id

--- src/main/java/com/andrebovis/workshopmongo/services/UserService.java
+++ src/main/java/com/andrebovis/workshopmongo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.andrebovis.workshopmongo.services;
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andrebovis.workshopmongo.domain.User;
 import com.andrebovis.workshopmongo.repository.UserRepository;
+import com.andrebovis.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,7 +19,11 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 		
-		
+	}	
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 	}
 
-}
