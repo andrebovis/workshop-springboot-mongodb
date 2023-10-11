d645e2808dc5524e6ccae267df63bc36c8cefc95
andre bovis <andre_bovis@hotmail.com>
2023-10-11 02:26:31 -0300

    insercao de usuario com post

--- src/main/java/com/andrebovis/workshopmongo/services/UserService.java
+++ src/main/java/com/andrebovis/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andrebovis.workshopmongo.domain.User;
+import com.andrebovis.workshopmongo.dto.UserDTO;
 import com.andrebovis.workshopmongo.repository.UserRepository;
 import com.andrebovis.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	public User insert (User obj) {
+		return repo.insert(obj);
 	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
 
