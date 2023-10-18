e6c301588c5625b5331541ab1a1149b388cc3e72
andre bovis <andre_bovis@hotmail.com>
2023-10-18 02:20:24 -0300

    referenciando posts

--- src/main/java/com/andrebovis/workshopmongo/config/Instantiation.java
+++ src/main/java/com/andrebovis/workshopmongo/config/Instantiation.java
@@ -39,6 +39,9 @@ public class Instantiation implements  CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para sp, abracos", new AuthorDTO(maria) );
 		Post post2 = new Post(null, sdf.parse("23/08/1954"), "para mano", "ah va", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 	
